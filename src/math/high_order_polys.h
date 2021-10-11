f6047ff38361e93678d70081af92898116d1c506
iconocl4st <you@example.com>
2021-10-10 21:51:09 -0600

    probably in the middle of something

--- src/math/high_order_polys.h
+++ src/math/high_order_polys.h
@@ -8,18 +8,83 @@
 #include <iostream>
 #include <Eigen/Dense>
 
+#include "billiards_common/geometry/tolerance.h"
+
 namespace billiards::shots::math {
 	inline
-	void foo() {
-		Eigen::Matrix3f A;
-		Eigen::Vector3f b;
-		A << 1,2,3,  4,5,6,  7,8,10;
-		b << 3, 3, 4;
-		std::cout << "Here is the matrix A:\n" << A << std::endl;
-		std::cout << "Here is the vector b:\n" << b << std::endl;
-		Eigen::Vector3f x = A.colPivHouseholderQr().solve(b);
-		std::cout << "The solution is:\n" << x << std::endl;
+	void compute_roots(
+		const std::vector<double>& coefficients,
+		const std::function<void(const double)>& receiver
+	) {
+		int degree = (int) coefficients.size() - 1;
+		while (degree > 0 && std::abs(coefficients[degree]) < TOLERANCE) {
+			--degree;
+		}
+		if (degree < 0) {
+			return;
+		} else if (degree == 0) {
+			if (std::abs(coefficients[0]) < TOLERANCE) {
+				receiver(0);
+			}
+			return;
+		}
+
+		const double leading = coefficients[degree];
+		Eigen::MatrixXd companion_matrix{degree, degree};
+		for (int i = 0; i < degree; i++) {
+			for (int j = 0; j < degree; j++) {
+				if (j == degree - 1) {
+					companion_matrix(i, j) = -coefficients[i] / leading;
+				} else if (i == j + 1) {
+					companion_matrix(i, j) = 1;
+				} else {
+					companion_matrix(i, j) = 0;
+				}
+			}
+		}
+
+		Eigen::EigenSolver<Eigen::MatrixXd> s(companion_matrix); // the instance s(A) includes the eigensystem
+		std::cout << "Found matrix: " << std::endl;
+		std::cout << companion_matrix << std::endl;
+
+		const auto& eigen_values = s.eigenvalues();
+		for (int i = 0; i < eigen_values.size(); i++) {
+			const double real = eigen_values[i].real();
+			const double imag = eigen_values[i].imag();
+			if (std::abs(imag) < TOLERANCE) {
+				receiver(real);
+			}
+		}
 	}
 }
+
+
+
+/*
+ *
+//		const double a0 = c0 / c6;
+//		const double a1 = c1 / c6;
+//		const double a2 = c2 / c6;
+//		const double a3 = c3 / c6;
+//		const double a4 = c4 / c6;
+//		const double a5 = c5 / c6;
+//
+//		C	<<	0, 0, 0, 0, 0, -a0,
+//				1, 0, 0, 0, 0, -a1,
+//				0, 1, 0, 0, 0, -a2,
+//				0, 0, 1, 0, 0, -a3,
+//				0, 0, 0, 1, 0, -a4,
+//				0, 0, 0, 0, 1, -a5;
+		std::cout << companion_matrix << std::endl;
+		std::cout << "eigenvalues:" << std::endl;
+		std::cout << s.eigenvalues() << std::endl;
+		std::cout << "eigenvectors=" << std::endl;
+		std::cout << s.eigenvectors() << std::endl;
+//		std::cout << s.eigenvalues()[0].
+//		std::cout << s.eigenvalues()[0] << std::endl;
+////		for (const auto v : s.eigenvalues()) {
+////			std::cout << v << std::endl;
+////		}
+ */
 
 #endif //IDEA_HIGH_ORDER_POLYS_H
--- src/math/rolling_glance.h
+++ src/math/rolling_glance.h
@@ -5,11 +5,12 @@
 #ifndef IDEA_ROLLING_GLANCE_H
 #define IDEA_ROLLING_GLANCE_H
 
-#include "billiards_common/math/polynomial.h"
+//#include "billiards_common/math/polynomial.h"
 #include "billiards_common/shots/RollingGlanceCalculation.h"
 #include "math/high_order_polys.h"
 
 namespace billiards::shots::math {
+
 
 	inline
 	void get_glance_location(
@@ -18,109 +19,146 @@ namespace billiards::shots::math {
 		const double r,
 		const std::function<void(const billiards::shots::RollingGlanceCalculation&)>& receiver
 	) {
-		foo();
+		const double dx_2 = dx * dx;
+		const double dy_2 = dy * dy;
+		const double ax_2 = ax * ax;
+		const double ax_3 = ax_2 * ax;
+		const double ax_4 = ax_3 * ax;
+		const double ay_2 = ay * ay;
+		const double ay_3 = ay_2 * ay;
+		const double ay_4 = ay_3 * ay;
+		const double r_2 = r * r;
+		const double r_3 = r_2 * r;
+		const double r_4 = r_3 * r;
+		const double r_5 = r_4 * r;
+		const double r_6 = r_5 * r;
 
-		const double ax2 = ax * ax;
-		const double ax3 = ax2 * ax;
-		const double ax4 = ax3 * ax;
-		const double ay2 = ay * ay;
-		const double ay3 = ay2 * ay;
-		const double ay4 = ay3 * ay;
-		const double dx2 = dx * dx;
-		const double dy2 = dy * dy;
-		const double r2 = r * r;
-		const double na2 = ax * ax + ay * ay;
-		if (std::abs(na2) < TOL) {
-			// cue is distance 0 away...
-			return;
-		}
-		if (std::abs(dx) < TOL && std::abs(dy) < TOL) {
-			// cue is at destination...
-			return;
-		}
+//		const double na2 = ax * ax + ay * ay;
+//		if (std::abs(na2) < TOLERANCE) {
+//			// cue is distance 0 away...
+//			return;
+//		}
+//		if (std::abs(dx) < TOLERANCE && std::abs(dy) < TOLERANCE) {
+//			// cue is at destination...
+//			return;
+//		}
 
 		const auto checker = [&](const RollingGlanceCalculation& rgc) {
+			const double x = rgc.loc.x;
+			const double y = rgc.loc.y;
 			const double tx = rgc.tan_dir.x;
 			const double ty = rgc.tan_dir.y;
 			const double aim_x = rgc.aim_dir.x;
 			const double aim_y = rgc.aim_dir.y;
 
-			const double s = (aim_x * tx + aim_y * ty) / (aim_x * aim_x + aim_y * aim_y);
 			const double alpha = 2 / 7.0;
 			double t;
-			if (std::abs(dx) >= TOL) {
-				t = (alpha * aim_x + (1 - alpha) * s * aim_x) / dx;
-			} else if (std::abs(dy) >= TOL) {
-				t = (alpha * aim_y + (1 - alpha) * s * aim_y) / dy;
+			if (std::abs(dx - x) >= TOLERANCE) {
+				t = (alpha * (tx - x) + (1 - alpha) * (aim_x - x)) / (dx - x);
+			} else if (std::abs(dy - y) >= TOLERANCE) {
+				t = (alpha * (ty - y) + (1 - alpha) * (aim_y - y)) / (dy - y);
 			} else {
 				throw std::runtime_error{"We already checked whether dx and dy are zero..."};
 			}
 
-			const double orthogonality_at_s = rgc.aim_dir.x * (s * rgc.aim_dir.x - tx) + rgc.aim_dir.y * (s * rgc.aim_dir.y - ty);
-			if (std::abs(orthogonality_at_s) > LARGER_TOL) {
+			const double orthogonality_at_s = (rgc.aim_dir.x - x) * (rgc.aim_dir.x - tx) + (rgc.aim_dir.y - y) * (rgc.aim_dir.y - ty);
+			std::cout << "orthogonality: " << orthogonality_at_s << std::endl;
+			if (std::abs(orthogonality_at_s) > CHECK_TOL) {
 				return;
 			}
 
-			const double rolling_x = alpha * tx + (1 - alpha) * s * aim_x - t * dx;
-			if (std::abs(rolling_x) > LARGER_TOL) {
+			const double rolling_x = alpha * (tx - x) + (1 - alpha) * (aim_x - x) - t * (dx - x);
+			std::cout << "rolling_x: " << rolling_x << std::endl;
+			if (std::abs(rolling_x) > CHECK_TOL) {
 				return;
 			}
-			const double rolling_y = alpha * ty + (1 - alpha) * s * aim_y - t * dy;
-			if (std::abs(rolling_y) > LARGER_TOL) {
+			const double rolling_y = alpha * (ty - y) + (1 - alpha) * (aim_y - y) - t * (dy - y);
+			std::cout << "rolling_y: " << rolling_y << std::endl;
+			if (std::abs(rolling_y) > CHECK_TOL) {
+				return;
+			}
+
+			const double t_same_direction = (tx - x) * (dx - x) + (ty - y) * (dy - y);
+			std::cout << "t same direction: " << t_same_direction << std::endl;
+			if (t_same_direction < 0) {
+				return;
+			}
+			const double d_same_direction = (tx - x) * (dx - x) + (ty - y) * (dy - y);
+			std::cout << "d same direction as t: " << d_same_direction << std::endl;
+			if (d_same_direction < 0) {
+				return;
+			}
+			const double a_same_direction = x * (ax - x) + y * (ay - y);
+			std::cout << "a same direction: " << a_same_direction << std::endl;
+			if (a_same_direction < 0) {
 				return;
 			}
 			receiver(rgc);
 		};
 
-		do {
-			const double c0 = -25/ (double) 49*ax2*ay2*dx2*r2 + 10/ (double) 7*ax3*ay*dx*dy*r2 + 20/ (double) 49*ax*ay3*dx*dy*r2 - ax4*dy2*r2 - 4/ (double) 7*ax2*ay2*dy2*r2 - 4/ (double) 49*ay4*dy2*r2;
-			const double c2 = 4/ (double) 49*ax4*dx2 + 53/ (double) 49*ax2*ay2*dx2 + ay4*dx2 - 90/ (double) 49*ax3*ay*dx*dy - 90/ (double) 49*ax*ay3*dx*dy + ax4*dy2 + 53/ (double) 49*ax2*ay2*dy2 + 4/ (double) 49*ay4*dy2;
-			if (std::abs(c2) < TOL) {
-				break;
-			}
-			const double x2 = -c0 / c2;
-			if (x2 < 0) {
-				break;
-			}
-			const double x = std::sqrt(x2);
-			const double rad = r * r - x * x;
-			double y;
-			if (rad > TOL) {
-				y = std::sqrt(rad);
-			} else if (rad > -TOL) {
-				y = 0;
-			} else {
-				break;
-			}
-			const double tx = y;
-			const double ty = -x;
-			checker(RollingGlanceCalculation{x, y, tx, ty, x - ax, y - ay});
-		} while (false);
+		{
+			const double a0 = -25/ (double) 49*ax_2*ay_2*dx_2*r_2 + 10/ (double) 7*ax_3*ay*dx*dy*r_2 + 20/ (double) 49*ax*ay_3*dx*dy*r_2 - ax_4*dy_2*r_2 - 4/ (double) 7*ax_2*ay_2*dy_2*r_2 - 4/ (double) 49*ay_4*dy_2*r_2 + ax_4*r_4 + 4/ (double) 7*ax_2*ay_2*r_4 + 4/ (double) 49*ay_4*r_4 - 10/ (double) 7*ax_3*dx*r_4 + 20/ (double) 49*ax*ay_2*dx*r_4 + 25/ (double) 49*ax_2*dx_2*r_4 - 20/ (double) 49*ax*ay*dx*dy*r_4 - 4/ (double) 7*ax_2*dy_2*r_4 + 8/ (double) 49*ay_2*dy_2*r_4 + 4/ (double) 7*ax_2*r_6 - 8/ (double) 49*ay_2*r_6 - 20/ (double) 49*ax*dx*r_6 - 4/ (double) 49*dy_2*r_6 + 4/ (double) 49*std::pow(r, 8);
+			const double a1 = -4/ (double) 7*ax_4*dx*r_2 - 6/ (double) 49*ax_2*ay_2*dx*r_2 - 4/ (double) 7*ay_4*dx*r_2 + 20/ (double) 49*ax_3*dx_2*r_2 - 20/ (double) 49*ax*ay_2*dx_2*r_2 - 10/ (double) 7*ax_3*ay*dy*r_2 - 20/ (double) 49*ax*ay_3*dy*r_2 - 30/ (double) 49*ax_2*ay*dx*dy*r_2 - 20/ (double) 49*ay_3*dx*dy*r_2 + 18/ (double) 7*ax_3*dy_2*r_2 + 76/ (double) 49*ax*ay_2*dy_2*r_2 - 8/ (double) 7*ax_3*r_4 - 96/ (double) 49*ax*ay_2*r_4 + 4/ (double) 49*ax_2*dx*r_4 + 36/ (double) 49*ay_2*dx*r_4 + 20/ (double) 49*ax*dx_2*r_4 + 20/ (double) 49*ax*ay*dy*r_4 + 20/ (double) 49*ay*dx*dy*r_4 + 36/ (double) 49*ax*dy_2*r_4 - 16/ (double) 49*ax*r_6 - 8/ (double) 49*dx*r_6;
+			const double a2 = 4/ (double) 49*ax_4*dx_2 + 53/ (double) 49*ax_2*ay_2*dx_2 + ay_4*dx_2 - 90/ (double) 49*ax_3*ay*dx*dy - 90/ (double) 49*ax*ay_3*dx*dy + ax_4*dy_2 + 53/ (double) 49*ax_2*ay_2*dy_2 + 4/ (double) 49*ay_4*dy_2 - 10/ (double) 7*ax_4*r_2 - 50/ (double) 49*ax_2*ay_2*r_2 + 20/ (double) 49*ay_4*r_2 + 192/ (double) 49*ax_3*dx*r_2 + 26/ (double) 7*ax*ay_2*dx*r_2 - 82/ (double) 49*ax_2*dx_2*r_2 - 53/ (double) 49*ay_2*dx_2*r_2 + 30/ (double) 49*ax_2*ay*dy*r_2 + 20/ (double) 49*ay_3*dy*r_2 - 40/ (double) 49*ax*ay*dx*dy*r_2 - 53/ (double) 49*ax_2*dy_2*r_2 - 64/ (double) 49*ay_2*dy_2*r_2 - 4/ (double) 49*ax_2*r_4 + 36/ (double) 49*ay_2*r_4 + 52/ (double) 49*ax*dx*r_4 + 4/ (double) 49*dx_2*r_4 - 20/ (double) 49*ay*dy*r_4 + 4/ (double) 49*dy_2*r_4;
+			const double a3 = 20/ (double) 49*ax_4*dx - 50/ (double) 49*ax_2*ay_2*dx - 10/ (double) 7*ay_4*dx - 36/ (double) 49*ax_3*dx_2 - 36/ (double) 49*ax*ay_2*dx_2 + 90/ (double) 49*ax_3*ay*dy + 90/ (double) 49*ax*ay_3*dy + 90/ (double) 49*ax_2*ay*dx*dy + 90/ (double) 49*ay_3*dx*dy - 18/ (double) 7*ax_3*dy_2 - 18/ (double) 7*ax*ay_2*dy_2 + 40/ (double) 49*ax_3*r_2 + 40/ (double) 49*ax*ay_2*r_2 - 52/ (double) 49*ax_2*dx*r_2 - 92/ (double) 49*ay_2*dx*r_2 - 36/ (double) 49*ax*dx_2*r_2 + 40/ (double) 49*ax*ay*dy*r_2 - 36/ (double) 49*ax*dy_2*r_2;
+			const double a4 = 25/ (double) 49*ax_4 + 50/ (double) 49*ax_2*ay_2 + 25/ (double) 49*ay_4 - 90/ (double) 49*ax_3*dx - 90/ (double) 49*ax*ay_2*dx + 81/ (double) 49*ax_2*dx_2 + 81/ (double) 49*ay_2*dx_2 - 90/ (double) 49*ax_2*ay*dy - 90/ (double) 49*ay_3*dy + 81/ (double) 49*ax_2*dy_2 + 81/ (double) 49*ay_2*dy_2;
+			std::vector<double> coefficients{a0, a1, a2, a3, a4};
 
-		do {
-			const double c0 = -25/ (double) 49*ax2*ay2*dx2*r2 + 10/ (double) 7*ax3*ay*dx*dy*r2 + 20/ (double) 49*ax*ay3*dx*dy*r2 - ax4*dy2*r2 - 4/ (double) 7*ax2*ay2*dy2*r2 - 4/ (double) 49*ay4*dy2*r2;
-			const double c2 = 4/ (double) 49*ax4*dx2 + 53/ (double) 49*ax2*ay2*dx2 + ay4*dx2 - 90/ (double) 49*ax3*ay*dx*dy - 90/ (double) 49*ax*ay3*dx*dy + ax4*dy2 + 53/ (double) 49*ax2*ay2*dy2 + 4/ (double) 49*ay4*dy2;
-			if (std::abs(c2) < TOL) {
-				break;
-			}
-			const double x2 = -c0 / c2;
-			if (x2 < 0) {
-				break;
-			}
-			const double x = std::sqrt(x2);
-			const double rad = r * r - x * x;
-			double y;
-			if (rad > TOL) {
-				y = std::sqrt(rad);
-			} else if (rad > -TOL) {
-				y = 0;
-			} else {
-				break;
-			}
-			const double tx = -y;
-			const double ty = x;
-			checker(RollingGlanceCalculation{x, y, tx, ty, x - ax, y - ay});
-		} while (false);
+			billiards::shots::math::compute_roots(coefficients, [&](const double x) {
+				const double rad = r * r - x * x;
+				double y;
+				if (rad > TOLERANCE) {
+					y = std::sqrt(rad);
+				} else if (rad > -TOLERANCE) {
+					y = 0;
+				} else {
+					return;
+				}
+				const double tx = x + y;
+				const double ty = -x + y;
+				const double s_numerator = ay*x - ax*y;
+				const double s_denominator = ax_2 + ay_2 - 2*ax*x + std::pow(x, 2) - 2*ay*y + std::pow(y, 2);
+				if (std::abs(s_denominator) < TOLERANCE) {
+					return;
+				}
+				const double s = s_numerator / s_denominator;
+				const double aim_x = -(ax - x)*s + x;
+				const double aim_y = -(ay - y)*s + y;
+				checker(RollingGlanceCalculation{x, y, tx, ty, aim_x, aim_y});
+			});
+		}
+
+		{
+			const double a0 = -25/ (double) 49*ax_2*ay_2*dx_2*r_2 + 10/ (double) 7*ax_3*ay*dx*dy*r_2 + 20/ (double) 49*ax*ay_3*dx*dy*r_2 - ax_4*dy_2*r_2 - 4/ (double) 7*ax_2*ay_2*dy_2*r_2 - 4/ (double) 49*ay_4*dy_2*r_2 + ax_4*r_4 + 4/ (double) 7*ax_2*ay_2*r_4 + 4/ (double) 49*ay_4*r_4 - 10/ (double) 7*ax_3*dx*r_4 + 20/ (double) 49*ax*ay_2*dx*r_4 + 25/ (double) 49*ax_2*dx_2*r_4 - 20/ (double) 49*ax*ay*dx*dy*r_4 - 4/ (double) 7*ax_2*dy_2*r_4 + 8/ (double) 49*ay_2*dy_2*r_4 + 4/ (double) 7*ax_2*r_6 - 8/ (double) 49*ay_2*r_6 - 20/ (double) 49*ax*dx*r_6 - 4/ (double) 49*dy_2*r_6 + 4/ (double) 49*std::pow(r, 8);
+			const double a1 = -4/ (double) 7*ax_4*dx*r_2 - 6/ (double) 49*ax_2*ay_2*dx*r_2 - 4/ (double) 7*ay_4*dx*r_2 + 20/ (double) 49*ax_3*dx_2*r_2 - 20/ (double) 49*ax*ay_2*dx_2*r_2 - 10/ (double) 7*ax_3*ay*dy*r_2 - 20/ (double) 49*ax*ay_3*dy*r_2 - 30/ (double) 49*ax_2*ay*dx*dy*r_2 - 20/ (double) 49*ay_3*dx*dy*r_2 + 18/ (double) 7*ax_3*dy_2*r_2 + 76/ (double) 49*ax*ay_2*dy_2*r_2 - 8/ (double) 7*ax_3*r_4 - 96/ (double) 49*ax*ay_2*r_4 + 4/ (double) 49*ax_2*dx*r_4 + 36/ (double) 49*ay_2*dx*r_4 + 20/ (double) 49*ax*dx_2*r_4 + 20/ (double) 49*ax*ay*dy*r_4 + 20/ (double) 49*ay*dx*dy*r_4 + 36/ (double) 49*ax*dy_2*r_4 - 16/ (double) 49*ax*r_6 - 8/ (double) 49*dx*r_6;
+			const double a2 = 4/ (double) 49*ax_4*dx_2 + 53/ (double) 49*ax_2*ay_2*dx_2 + ay_4*dx_2 - 90/ (double) 49*ax_3*ay*dx*dy - 90/ (double) 49*ax*ay_3*dx*dy + ax_4*dy_2 + 53/ (double) 49*ax_2*ay_2*dy_2 + 4/ (double) 49*ay_4*dy_2 - 10/ (double) 7*ax_4*r_2 - 50/ (double) 49*ax_2*ay_2*r_2 + 20/ (double) 49*ay_4*r_2 + 192/ (double) 49*ax_3*dx*r_2 + 26/ (double) 7*ax*ay_2*dx*r_2 - 82/ (double) 49*ax_2*dx_2*r_2 - 53/ (double) 49*ay_2*dx_2*r_2 + 30/ (double) 49*ax_2*ay*dy*r_2 + 20/ (double) 49*ay_3*dy*r_2 - 40/ (double) 49*ax*ay*dx*dy*r_2 - 53/ (double) 49*ax_2*dy_2*r_2 - 64/ (double) 49*ay_2*dy_2*r_2 - 4/ (double) 49*ax_2*r_4 + 36/ (double) 49*ay_2*r_4 + 52/ (double) 49*ax*dx*r_4 + 4/ (double) 49*dx_2*r_4 - 20/ (double) 49*ay*dy*r_4 + 4/ (double) 49*dy_2*r_4;
+			const double a3 = 20/ (double) 49*ax_4*dx - 50/ (double) 49*ax_2*ay_2*dx - 10/ (double) 7*ay_4*dx - 36/ (double) 49*ax_3*dx_2 - 36/ (double) 49*ax*ay_2*dx_2 + 90/ (double) 49*ax_3*ay*dy + 90/ (double) 49*ax*ay_3*dy + 90/ (double) 49*ax_2*ay*dx*dy + 90/ (double) 49*ay_3*dx*dy - 18/ (double) 7*ax_3*dy_2 - 18/ (double) 7*ax*ay_2*dy_2 + 40/ (double) 49*ax_3*r_2 + 40/ (double) 49*ax*ay_2*r_2 - 52/ (double) 49*ax_2*dx*r_2 - 92/ (double) 49*ay_2*dx*r_2 - 36/ (double) 49*ax*dx_2*r_2 + 40/ (double) 49*ax*ay*dy*r_2 - 36/ (double) 49*ax*dy_2*r_2;
+			const double a4 = 25/ (double) 49*ax_4 + 50/ (double) 49*ax_2*ay_2 + 25/ (double) 49*ay_4 - 90/ (double) 49*ax_3*dx - 90/ (double) 49*ax*ay_2*dx + 81/ (double) 49*ax_2*dx_2 + 81/ (double) 49*ay_2*dx_2 - 90/ (double) 49*ax_2*ay*dy - 90/ (double) 49*ay_3*dy + 81/ (double) 49*ax_2*dy_2 + 81/ (double) 49*ay_2*dy_2;
+			std::vector<double> coefficients{a0, a1, a2, a3, a4};
+
+			billiards::shots::math::compute_roots(coefficients, [&](const double x) {
+				const double rad = r * r - x * x;
+				double y;
+				if (rad > TOLERANCE) {
+					y = std::sqrt(rad);
+				} else if (rad > -TOLERANCE) {
+					y = 0;
+				} else {
+					return;
+				}
+				const double tx = x - y;
+				const double ty = x + y;
+				const double s_numerator = -ay*x + ax*y;
+				const double s_denominator = ax_2 + ay_2 - 2*ax*x + std::pow(x, 2) - 2*ay*y + std::pow(y, 2);
+				if (std::abs(s_denominator) < TOLERANCE) {
+					return;
+				}
+				const double s = s_numerator / s_denominator;
+				const double aim_x = -(ax - x)*s + x;
+				const double aim_y = -(ay - y)*s + y;
+				checker(RollingGlanceCalculation{x, y, tx, ty, aim_x, aim_y});
+			});
+		}
 	}
 
 
